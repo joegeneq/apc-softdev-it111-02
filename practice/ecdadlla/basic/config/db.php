29d6695c531d4128db48243269d97f9786c38fd0
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-02-16 21:27:15 +0800

    Cookies, updated dbname,username,password

--- practice/ecdadlla/basic/config/db.php
+++ practice/ecdadlla/basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=jfkuniversal',
+    'username' => 'jfkuniversal',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
