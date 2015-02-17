2dfa60208d110918846dcd5be20a979c6e0e4f1b
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-02-17 20:00:42 +0800

    Re-extracted basic zip file. Some files are missing

--- practice/ecdadlla/basic/config/db.php
+++ practice/ecdadlla/basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jfkuniversal',
-    'username' => 'jfkuniversal',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 ];
