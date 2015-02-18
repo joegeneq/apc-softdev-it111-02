3a4ece87a8acc46e59b575650d47623252f4a163
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-02-18 15:44:21 +0800

    Created region model and crud

--- practice/ecdadlla/basic/config/db.php
+++ practice/ecdadlla/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=jfkyii',
+    'dsn' => 'mysql:host=localhost;dbname=it111_exercise',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
