4ede919bc94093286f5eccec6f8710c733e05d0c
Katharyn Alexandre D. Mayo <1katiebrit@gmail.com>
2015-02-28 11:28:00 +0800

    created crud and controller for mycomment

--- practice/midterm_test/Mayo_Katharyn_Alexandre_midterms/ourspace/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=softdev_mayo_katharyn_ourspace',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
