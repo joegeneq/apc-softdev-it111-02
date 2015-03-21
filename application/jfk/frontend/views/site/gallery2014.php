573fa31a54f483d4aa1eb77758e251effb31b4ea
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-03-21 23:41:12 +0800

    Worked On Gallery images Resized some of the images since the resolution is big Fixed gallery initial layout and style

--- application/jfk/frontend/views/site/gallery2014.php
+++ application/jfk/frontend/views/site/gallery2014.php
@@ -2,14 +2,143 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = '2014Gallery';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = '2014 Gallery';
+
 ?>
 <div class="site-events">
 		</br>
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
+   <!--THE IMAGES ARE PLACED IN AN UNORDERED LIST-->
+    <ul class="enlarge"> <!--We give the list a class so that we can style it seperately from other unordered lists-->
+
+    <!--First Image-->
+    <li>
+    <img src="images/jfk2014/1.jpg" width="200px" height="150px" alt="1" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/1.jpg" alt="1" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!--Second Image-->
+    <li>
+    <img src="images/jfk2014/2.jpg" width="200px" height="150px" alt="2" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/2.jpg" alt="2" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!--Third Image-->
+    <li>
+    <img src="images/jfk2014/3.jpg" width="200px" height="150px" alt="3" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/3.jpg" alt="3" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Fourth Image-->
+     <li>
+    <img src="images/jfk2014/4.jpg" width="200px" height="150px" alt="4" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/4.jpg" alt="4" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Fifth Image -->
+    <li>
+    <img src="images/jfk2014/5.jpg" width="200px" height="150px" alt="5" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/5.jpg" alt="5" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/6.jpg" width="200px" height="150px" alt="6" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/6.jpg" alt="6" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/7.jpg" width="200px" height="150px" alt="7" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/7.jpg" alt="7" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/8.jpg" width="200px" height="150px" alt="8" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/8.jpg" alt="8" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/9.jpg" width="200px" height="150px" alt="9" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/9.jpg" alt="9" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/10.jpg" width="200px" height="150px" alt="10" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/10.jpg" alt="10" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/11.jpg" width="200px" height="150px" alt="11" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/11.jpg" alt="11" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/12.jpg" width="200px" height="150px" alt="12" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/12.jpg" alt="12" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/13.jpg" width="200px" height="150px" alt="13" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/13.jpg" alt="13" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    <!-- Sixth Image -->
+    <li>
+    <img src="images/jfk2014/14.jpg" width="200px" height="150px" alt="14" /> <!--thumbnail image-->
+    <span> <!--span contains the popup image-->
+    <img src="images/jfk2014/14.jpg" alt="14" /> <!--popup image-->
+    <br /> <!--caption appears under the popup image-->
+    </span>
+    </li>
+
+    </ul>
   
 
    
