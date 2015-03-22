09cdbf2ee0febe2ac3c6e882bef4371cec86ca00
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-22 13:45:19 +0800

    Revert "Worked On Gallery images"
    This reverts commit 573fa31a54f483d4aa1eb77758e251effb31b4ea.

--- application/jfk/frontend/views/site/gallery2013.php
+++ application/jfk/frontend/views/site/gallery2013.php
@@ -2,110 +2,25 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = '2013 Gallery';
-
+$this->title = '2013Gallery';
+$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-events">
 		</br>
     <h1><?= Html::encode($this->title) ?></h1>
-<!--
-<table>
-	<tr class="gallery-tr">
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/1.jpg" />
-			</div>
-		</td>
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/2.jpg" />
-			</div>
-		</td>
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/3.jpg" />
-			</div>
-		</td>
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/4.jpg" />
-			</div>
-		</td>
-	<tr>
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/5.jpg" />
-			</div>
-		</td>
-		<td class="zoom-img">
-			<div class="effectback">
-    		<img class="effectfront" src="images/xmas/6.jpg"  />
-			</div>
-		</td>
-	</tr>
-	</tr>
-</table> 
-!-->
+	<div class="col-lg-4">
+                
+                   <?= Html::img('images/xmas/1.jpg');?> <br>
+				   <?= Html::img('images/xmas/2.jpg');?> <br>
+					<?= Html::img('images/xmas/3.jpg');?> <br>
+					<?= Html::img('images/xmas/4.jpg');?> <br>
+				</br>
+				</br>
+                
+            </div>
 
+    <p>
+  
 
-    <!--THE IMAGES ARE PLACED IN AN UNORDERED LIST-->
-    <ul class="enlarge"> <!--We give the list a class so that we can style it seperately from other unordered lists-->
-
-    <!--First Image-->
-    <li>
-    <img src="images/xmas/1.jpg" width="200px" height="150px" alt="1" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/1.jpg" alt="1" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    <!--Second Image-->
-    <li>
-    <img src="images/xmas/2.jpg" width="200px" height="150px" alt="2" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/2.jpg" alt="2" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    <!--Third Image-->
-    <li>
-    <img src="images/xmas/3.jpg" width="200px" height="150px" alt="3" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/3.jpg" alt="3" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    <!-- Fourth Image-->
-     <li>
-    <img src="images/xmas/4.jpg" width="200px" height="150px" alt="4" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/4.jpg" alt="3" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    <!-- Fifth Image -->
-    <li>
-    <img src="images/xmas/5.jpg" width="200px" height="150px" alt="5" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/5.jpg" alt="3" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    <!-- Sixth Image -->
-    <li>
-    <img src="images/xmas/6.jpg" width="200px" height="150px" alt="6" /> <!--thumbnail image-->
-    <span> <!--span contains the popup image-->
-    <img src="images/xmas/6.jpg" alt="3" /> <!--popup image-->
-    <br /> <!--caption appears under the popup image-->
-    </span>
-    </li>
-
-    </ul>
-
-
+   
 </div>
