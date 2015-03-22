ac1a09d9efffb62470bf5e13663b1c67a9e1b6f2
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-03-22 19:52:10 +0800

    Added item descriptions

--- application/jfk/frontend/views/site/shop.php
+++ application/jfk/frontend/views/site/shop.php
@@ -2,8 +2,8 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* $this->title = 'Shop';
-$this->params['breadcrumbs'][] = $this->title; */
+ $this->title = 'Shop';
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 
 
@@ -13,36 +13,56 @@ $this->params['breadcrumbs'][] = $this->title; */
 				<div class="col-lg-17">
 	
 					<div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>JFK Shirt</h2>
-
-                <?= Html::img('images/shirt.jpg');?>
-				</br>
-				</br>
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Buy &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>JFK Mug</h2>
-
-                <?= Html::img('images/cap.jpg');?>
-				</br>
-				</br>
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Buy &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>JFK Cap</h2>
-
-                <?= Html::img('images/mug.jpg');?>
-				</br>
-				</br>
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Buy &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
-					</div>
-					</div>
-
+		
+		<div class="row">
+			<div class="col-lg-4">
+				<table class="jfk-items">
+					<tr align="center" class="items">
+						<td>
+							<?= Html::img('images/shirt.jpg');?>
+						</td>
+							
+						<td>
+							<?= Html::img('images/cap.jpg');?>
+						</td>
+						<td>
+							<?= Html::img('images/mug.jpg');?>
+						</td>
+					</tr>
+					<tr align="center" class="item-def">
+						<td>
+							<b>JFK T-Shirt</b><br><br>
+							The only JFK product than can bought with variety of colors<br>
+							People are to choose what color of t-shirt they want upon purchasing.<br><br>
+							The JFK logo is located at the right chest of the T-shirt<br><br>
+			
+							This product is also available on 3 different sizes (S/M/L)<br>
+							Kindly contact us for sizes that are not mentioned.
+						</td>
+							
+						<td>
+							<b>JFK Cap</b><br><br>
+							A black colored cap that goes well with the blue JFK logo<br>
+							placed on the middle front of the item.<br><br>
+							
+							This product would be able to help volunteers and staff<br>
+							To withstand the heat while helping in our organization.<br><br>
+							Wear this and stand tall, let them know that you are helping<br>
+							the children of tomorrow.
+							
+						</td>
+						<td>
+							<b>JFK Mug</b><br><br>
+							Now you can have a cup with the organization's name<br><br>
+							
+							A mug that is helpful for everyday use. Cheers!~
+							
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		</div>
+		</div>
+		</div>
 </div>
