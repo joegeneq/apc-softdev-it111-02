c443cb2ea321d9c33bd34371b8dffb6e56442e3e
Katharyn Alexandre D. Mayo <1katiebrit@gmail.com>
2015-03-28 15:37:25 +0800

    Started and Finished Online Chat

--- application/jfk/frontend/views/site/index.php
+++ application/jfk/frontend/views/site/index.php
@@ -120,3 +120,8 @@ $this->title = 'Joy For Kids';
       </aside>
     </div>
 </div>
+
+<script type='text/javascript'>(function () { var done = false;var script = document.createElement('script');script.async = true;script.type = 
+'text/javascript';script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript';document.getElementsByTagName('HEAD').item(0).appendChild(script)
+;script.onreadystatechange = script.onload = function (e) {if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) 
+{var w = new PCWidget({ c: '0b88eb7b-9b74-4657-b17e-2649539ce375', f: true });done = true;}};})();</script>
