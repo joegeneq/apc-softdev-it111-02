ea5abbd959ce9498bfbc3ba0befc1f0f1e63879a
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-30 04:17:13 +0800

    Fixed buttons for Shopping Cart
    Fixed buttons for Shopping Cart

--- application/jfk/frontend/views/site/shop.php
+++ application/jfk/frontend/views/site/shop.php
@@ -20,34 +20,9 @@ $this->params['breadcrumbs'][] = $this->title; */
 					<div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>JFK Shirt</h2>
-				
-                <?= Html::img('images/shirt.jpg');?><p>
-				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-				<input type="hidden" name="cmd" value="_s-xclick">
-				<input type="hidden" name="hosted_button_id" value="46MFW2T8YY3WJ">
-				<table>
-					<tr><td><input type="hidden" name="on0" value="White - Size">White - Size</td></tr><tr><td><select name="os0">
-				
-						<option value="White - Small">White - Small P160.00 PHP</option>
-						<option value="White - Medium">White - Medium P160.00 PHP</option>
-						<option value="White - Large">White - Large P160.00 PHP</option>
-						
-				</select> </td></tr>
-				</table>
-				<br>
-				<input type="hidden" name="currency_code" value="PHP">
-				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-				</form>
-
-                
-				
-				</p>
-            </div>
-            <div class="col-lg-4">
-                <h2>JFK Mug</h2>
+		
+		            <div class="col-lg-4">
+                <h2>JFK Cap</h2>
 
                 <?= Html::img('images/cap.jpg');?>
                 <p>
@@ -70,8 +45,72 @@ $this->params['breadcrumbs'][] = $this->title; */
 
 				</p>
             </div>
+		
+		
             <div class="col-lg-4">
-                <h2>JFK Cap</h2>
+                <h2>JFK Shirt</h2>
+				
+                <?= Html::img('images/shirt.jpg');?><p>
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="46MFW2T8YY3WJ">
+				<table>
+					<tr><td><input type="hidden" name="on0" value="White - Size">White - Size</td></tr><tr><td><select name="os0">
+				
+						<option value="White - Small">White - Small P160.00 PHP</option>
+						<option value="White - Medium">White - Medium P160.00 PHP</option>
+						<option value="White - Large">White - Large P160.00 PHP</option>
+						
+				</select> </td></tr>
+				</table>
+				<br>
+				<input type="hidden" name="currency_code" value="PHP">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+                _________________________________________________
+				<br>
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="FQUU7AKPKCUNC">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Sky Blue - Size">Sky Blue - Size</td></tr><tr><td><select name="os0">
+				<br>
+						<option value="Sky Blue - Small">Sky Blue - Small P160.00 PHP</option>
+						<option value="Sky Blue - Medium">Sky Blue - Medium P160.00 PHP</option>
+						<option value="Sky Blue - Large">Sky Blue - Large P160.00 PHP</option>
+				</select> </td></tr>
+				</table>
+				<br>
+				<input type="hidden" name="currency_code" value="PHP">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+                ________________________________________________
+				<br>
+                
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="2UG4LRDSHWN3A">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Black - Size">Black - Size</td></tr><tr><td><select name="os0">
+				<br>	
+					<option value="Black - Small">Black - Small P160.00 PHP</option>
+					<option value="Black - Medium">Black - Medium P160.00 PHP</option>
+					<option value="Black - Large">Black - Large P160.00 PHP</option>
+				</select> </td></tr>
+				</table>
+				<br>
+				<input type="hidden" name="currency_code" value="PHP">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+
+				</p>
+            </div>
+
+            <div class="col-lg-4">
+                <h2>JFK Mug</h2>
 
                 <?= Html::img('images/mug.jpg');?>
 				
