5a7cff7fd525889e925beb37473f9a1932231e05
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-30 03:26:44 +0800

    Inserted Shopping buttons for Mug and Cap
    Inserted Shopping buttons for Mug and Cap

--- application/jfk/frontend/views/site/shop.php
+++ application/jfk/frontend/views/site/shop.php
@@ -12,12 +12,6 @@ $this->params['breadcrumbs'][] = $this->title; */
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <div class="site-shop">
 	
 	 <div class="row">
@@ -40,31 +34,51 @@ $this->params['breadcrumbs'][] = $this->title; */
                 <h2>JFK Mug</h2>
 
                 <?= Html::img('images/cap.jpg');?>
-				</br>
-				</br>
                 <p>
-				
+				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="7BGMSDDV6PF5Q">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Color">Color</td></tr><tr><td><select name="os0">
+				<br>
+					<option value="White">White P150.00 PHP</option>
+					<option value="Sky Blue">Sky Blue P150.00 PHP</option>
+					<option value="Black">Black P150.00 PHP</option>
+				</select> </td></tr>
+				</table>
+				<br>
+				<input type="hidden" name="currency_code" value="PHP">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+
 				</p>
             </div>
             <div class="col-lg-4">
                 <h2>JFK Cap</h2>
 
                 <?= Html::img('images/mug.jpg');?>
-				</br>
-				</br>
+				
                 <p>
 				<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="486HKP9LDXSPL">
-<table>
-<tr><td><input type="hidden" name="on0" value="Color">Color</td></tr><tr><td><select name="os0">
-	<option value="White">White P80.00 PHP</option>
-	<option value="Sky Blue">Sky Blue P80.00 PHP</option>
-</select> </td></tr>
-</table>
-<input type="hidden" name="currency_code" value="PHP">
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="XSRVL38PJF584">
+				<table>
+				<tr><td><input type="hidden" name="on0" value="Color">Color</td></tr>
+				
+				
+				<tr><td><select name="os0">
+					<option value="White">White P80.00 PHP</option>
+					<option value="Sky Blue">Sky Blue P80.00 PHP</option>
+					<option value="Black">Black P80.00 PHP</option>
+				</select> </td></tr>
+				</table>
+
+				<br>
+				
+				<input type="hidden" name="currency_code" value="PHP">
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
 
 				</p>
