28d744d3ed49fa8db986c6db8927c12f822aa7f3
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-30 04:18:22 +0800

    Create temporary Donate Page
    Create temporary Donate Page. The page is supposedly will redirect to
    Paypal once clicked.

--- application/jfk/frontend/views/site/donate.php
+++ application/jfk/frontend/views/site/donate.php
@@ -2,8 +2,8 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'Donate';
-$this->params['breadcrumbs'][] = $this->title;
+//$this->title = 'Donate';
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-donate">
 		</br>
@@ -12,8 +12,16 @@ $this->params['breadcrumbs'][] = $this->title;
 	
 	
     <p>
-        This is the Donate page. You may modify the following file to customize its content:
+	Click 
+       				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="BZQ5N3UKRZF4Y">
+				<input type="image" src="http://i57.tinypic.com/aynitv.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				</form>
+				
+	to redirect to Paypal.
     </p>
 
-    <code><?= __FILE__ ?></code>
+   
 </div>
