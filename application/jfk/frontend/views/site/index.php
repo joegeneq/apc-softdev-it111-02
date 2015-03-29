0dc81e09d350be697efb24e9ac510e755c499c44
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-29 14:32:15 +0800

    Changed button appearance
    Changed button appearance

--- application/jfk/frontend/views/site/index.php
+++ application/jfk/frontend/views/site/index.php
@@ -71,13 +71,13 @@ $this->title = 'Joy For Kids';
                 <!--<p><a class="btn btn-default" href="http://localhost/jfk/frontend/web/index.php?r=site%2Fdonate">Donate &raquo;</a></p>-->
 				
 				<p>
-				
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-					<input type="hidden" name="cmd" value="_s-xclick">
-					<input type="hidden" name="hosted_button_id" value="XVAZL3TFJQF7S">
-					<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-					<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+				<input type="hidden" name="cmd" value="_s-xclick">
+				<input type="hidden" name="hosted_button_id" value="BZQ5N3UKRZF4Y">
+				<input type="image" src="http://i57.tinypic.com/aynitv.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
+
 
 				
 				<!--Button to Dummy Account-->
