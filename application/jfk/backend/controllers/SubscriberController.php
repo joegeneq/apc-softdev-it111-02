e0092e9b1b24a9ff62b325fc44c3eccaf8094d38
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-03-29 23:34:58 +0800

    Restrict Subscriber Page if not Login
    Restrict Subscriber Page if not Login

--- application/jfk/backend/controllers/SubscriberController.php
+++ application/jfk/backend/controllers/SubscriberController.php
@@ -8,6 +8,7 @@ use backend\models\SubscriberDetails;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * SubscriberController implements the CRUD actions for Subsciber model.
@@ -17,6 +18,21 @@ class SubscriberController extends Controller
     public function behaviors()
     {
         return [
+		'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['index','_form','create','_search','update','view'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+		
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
