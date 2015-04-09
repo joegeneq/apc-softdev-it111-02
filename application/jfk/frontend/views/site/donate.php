381aaaf0c027ef29279082b940f9d7a8873d0e24
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-04-09 23:06:29 +0800

    Included other ways of donating
    Included other ways of donating

--- application/jfk/frontend/views/site/donate.php
+++ application/jfk/frontend/views/site/donate.php
@@ -9,19 +9,36 @@ use yii\helpers\Html;
 		</br>
     <h1><?= Html::encode($this->title) ?></h1>
 
-	
-	
-    <p>
-	Click 
-       				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>	
+   	        <div class="row">
+			
+				<div class="col-lg-4"> 
+				<h3> Donate thru Bank Transfer</h3><br>
+				<p> Call +63 27369686 or email info@jfkuniversal.org to ask for JFK's account details</p>
+				</div>
+				<div class="col-lg-4"> 
+				<h3>Donate thru PayPal with or without PayPal account</h3><br>
+       			<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_s-xclick">
 				<input type="hidden" name="hosted_button_id" value="BZQ5N3UKRZF4Y">
 				<input type="image" src="http://i57.tinypic.com/aynitv.jpg" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
-				
-	to redirect to Paypal.
-    </p>
+				</div>
+				<div class="col-lg-4"> 
+				<h3>Donate Directly</h3>
+				<p> You can go or mail your donation at <br>
+				JFK's office at UG-12 Cityland Pioneer Condominium, 128 Pioneer Street, Mandaluyong 
+				to personally give your donation to the foundation
+				</div>
+			</div>
 
    
 </div>
