c2713ca0824c47b23a22bf76f588a97bcd1fe9ce
Renon Errol Dadulla <ecdadlla@gmail.com>
2015-04-10 12:56:37 +0800

    Category Model, Category CRUD

---
+++ application/jfk/backend/models/Category.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "category".
+ *
+ * @property integer $id
+ * @property string $category
+ *
+ * @property Order[] $orders
+ * @property Productdetails[] $productdetails
+ * @property Products[] $products
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['category'], 'required'],
+            [['category'], 'string', 'max' => 45]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'category' => Yii::t('app', 'Category'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getOrders()
+    {
+        return $this->hasMany(Order::className(), ['category_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductdetails()
+    {
+        return $this->hasMany(Productdetails::className(), ['category_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Products::className(), ['category_id' => 'id']);
+    }
+}
