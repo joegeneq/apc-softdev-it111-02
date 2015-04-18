d9ca2c7754349d5689a96a549b32e8b8cdf4859e
Lois Anne G. Logronio <lloisanne00@gmail.com>
2015-04-19 03:18:58 +0800

    Made Drag and drop to insert many files
    Made Drag and drop to insert many files

--- application/jfk/backend/views/uploadpicture/_form.php
+++ application/jfk/backend/views/uploadpicture/_form.php
@@ -5,6 +5,7 @@ use yii\widgets\ActiveForm;
 use kartik\widgets\FileInput;
 use yii\helpers\ArrayHelper;
 use backend\models\Events;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model backend\models\Uploadpicture */
@@ -17,21 +18,27 @@ use backend\models\Events;
 
 
     <?= $form->field($model, 'events_eventID')->dropDownList(ArrayHelper::map(Events::find()->all(), 'eventID','EventName'),
-        [
+        [ 'prompt'=>'Select Event', ])->label('Event') ?>
 
-            'prompt'=>'Select Event',
-            /*'onchange'=>
-                '$.post
-                    ("index.php?r=products/category&id='.'"+$(this).val(), 
-                        function( data ) 
-                        {
-                            $( "select#order-products_id").html( data );
-                        }
-                    )'*/
-        ])->label('Event') ?>
+    <?= $form->field($model, 'file')->fileInput(['multiple'=>true]) ?>
 
-    <?= $form->field($model, 'file[]')->widget(FileInput::classname(), ['options'=>['multiple' => true]]); ?>
-
+	<?php
+    echo FileInput::widget([
+        'name' => 'uploadedFile[]',
+        'options'=>[
+            'multiple'=>true
+        ],
+        'pluginOptions' => [
+            'uploadUrl' => Url::to(['/site/file-upload']),
+            'uploadExtraData' => [
+                'album_id' => 20,
+                'cat_id' => 'Nature'
+            ],
+            'maxFileCount' => 10
+        ]
+    ])
+    ;?>
+	
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
